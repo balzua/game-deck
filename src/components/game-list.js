15abbb9f2a83efcc2dc9c997c9bd34d85d96c66a
Glitch (balzua-game-deck) <none>
2019-02-14 17:12:06 +0000

    game-list uses react redux and dummy state data

--- src/components/game-list.js
+++ src/components/game-list.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import './styles/game-list.css';
+import {connect} from 'react-redux';
 
+import './styles/game-list.css';
 import Game from './game';
 
-export default class GameList extends React.Component {
-  render() {
-    return (
-      <div className="items">
-        <Game title="Resident Evil 2" rating="M" description="Description" />
-        <Game title="Halo 3" rating="M" />
-      </div>
-    );
-  }
+export function GameList(props) {
+  const games = props.games.map((game, index) => 
+    <Game {...game} index={index} />
+  );
+  return (
+    <div className="items">
+      {games}
+    </div>
+  );
 };
+
+export const mapStateToProps = state => ({
+  games: state.games
+});
+
+export default connect(mapStateToProps)(GameList);
--- src/components/game.js
+++ src/components/game.js
@@ -3,27 +3,31 @@ import './styles/game.css';
 
 import GameStatus from './game-status';
 import DeleteButton from './delete-button';
+import Platform from './platform';
 
 export default class Game extends React.Component {
   render() {
+    const genres = this.props.genres.join(', ');
+    const platforms = this.props.platforms.map((platform, index) => 
+      <Platform name={platform} index={index} />
+    );
     return (
       <div className="library-item">
           <div className="game-title">
             <h3>{this.props.title}</h3>
           </div>
           <div className="game-img">
-            <img src="https://www.giantbomb.com/api/image/scale_small/2920687-the%20legend%20of%20zelda%20-%20breath%20of%20the%20wild%20v7.jpg" />
+            <img src={this.props.image} alt="Game Box Art" />
           </div>
           <div className="game-info">
+            <p>
+              {this.props.description}
+            </p>
             <ul>
-              <li><p>
-                Here is a description of the game which might be kind of long.
-                In fact it might even be 2 or 3 sentences long. This isn't uncommon.
-                </p></li>
               <li><b>Rating:</b> {this.props.rating}</li>
-              <li><b>Platform(s):</b> NS WiiU</li>
-              <li><b>Genre:</b> Action/Adventure</li>
-              <li><b>Released:</b> March 3, 2017</li>
+              <li><b>Platform(s): {platforms}</b> </li>
+              <li><b>Genre:</b> {genres}</li>
+              <li><b>Released:</b> {this.props.releaseDate}</li>
             </ul>
           </div>
           <div className="user-info">
