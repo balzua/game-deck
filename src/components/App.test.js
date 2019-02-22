35be3a59be4a9e1a923b75b7f18976b3bda2a889
balzua <43519244+balzua@users.noreply.github.com>
2019-02-22 14:55:05 -0600

    Update App.test.js

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -5,7 +5,5 @@ import App from './App';
 import {store} from '../store';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Provider store={store}><App /></Provider>, div);
-  ReactDOM.unmountComponentAtNode(div);
+
 }); 
