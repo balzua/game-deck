9571fa91b54020973e8e164e731ef1b4078f58a8
Glitch (balzua-game-deck-1) <none>
2019-02-28 16:11:54 +0000

    Release Date formatted

--- src/components/game.js
+++ src/components/game.js
@@ -7,7 +7,11 @@ export default function Game(props) {
   const platforms = props.platforms.map((platform, index) => 
     <Platform name={platform} key={index} />
   );
-                                             
+                                        
+  //Format the release date:
+  const releaseDate = new Date(props.releaseDate);
+  const dateString = `${releaseDate.getMonth() + 1}/${releaseDate.getDate() + 1}/${releaseDate.getFullYear()}`;
+                                        
   return (
     <React.Fragment>
       <div className="game-title">
@@ -24,7 +28,7 @@ export default function Game(props) {
           <li><b>Rating:</b> {props.rating}</li>
           <li><b>Platform(s): {platforms}</b> </li>
           <li><b>Genre:</b> {genres}</li>
-          <li><b>Released:</b> {props.releaseDate}</li>
+          <li><b>Released:</b> {dateString}</li>
         </ul>
       </div>
     </React.Fragment>
