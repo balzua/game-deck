b70ec74d8d9e29513cf43f7e605a67ffe5375d44
balzua <43519244+balzua@users.noreply.github.com>
2019-03-28 09:46:21 -0500

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ import './styles/footer.css';
 export default function Footer() {
   return (
   <footer>
-    <a href="https://github.com/balzua"><img src="https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png" height="40" width="40" /></a>
+    <a href="https://github.com/balzua"></a>
     App built by <a href="https://github.com/balzua">Brian Alzua</a> |
     Data and images provided by <a href="https://giantbomb.com/api">Giant Bomb</a>
   </footer>
